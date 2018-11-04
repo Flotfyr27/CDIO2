41e9da31a1ebb189ea081f21624556bd18697067
Fredrik <Fredrik200619@hotmail.com>
2018-11-04 17:29:57 +0100

    tilføjet die class

---
+++ src/main/java/Die.java
@@ -0,0 +1,22 @@
+package java;
+import java.security.SecureRandom;
+public class Die {
+    //use this perhaps: private static SecureRandom random = new SecureRandom(); is more random than other methods.
+
+    private int faceValue;
+
+    public Die(){
+        RollDie();
+    }
+
+    public int RollDie(){
+        faceValue = (int) ((Math.random() * 6) + 1);
+        return faceValue;
+    }
+
+    public int GetFaceValue(){
+        return faceValue;
+    }
+}
+
+
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,5 @@
-package spil;
-public class Player {
+package java;
+public class Player{
 
         private static Roll roll = new Roll();
         private static int numberOfPlayers = 0;
@@ -11,44 +11,45 @@ public class Player {
 
         private String rollLog;
 
-//* using switch statement to AddScore
+// using switch statement to AddScore
+    // switch engelsk
         public void main() {
-            int roll = score;
+            int roll = faceValue;
                 String scoreString;
                 switch (score) {
-                    case1: scoreString = "invalid number";
+                    case 1: scoreString = "invalid number";
                     break;
-                    case2: scoreString = "Tower + 250";
+                    case 2: scoreString = "Tower + 250";
                     score+=roll.GetAddedScore()=+250;
                     break;
-                    case3: scoreString = "Crater -100";
+                    case 3: scoreString = "Crater -100";
                     score+=roll.GetAddedScore()=-100;
                     break;
-                    case4: scoreString = "Palace gates + 100";
+                    case 4: scoreString = "Palace gates + 100";
                     score+=roll.GetAddedScore()= +100;
                     break;
-                    case5: scoreString = "Cold Desert -20";
+                    case 5: scoreString = "Cold Desert -20";
                     score+=roll.GetAddedScore()=-20;
                     break;
-                    case6: scoreString = "Walled city +180";
+                    case 6: scoreString = "Walled city +180";
                     score+=roll.GetAddedScore()+180;
                     break;
-                    case7: scoreString = "Monastry 0";
+                    case 7: scoreString = "Monastry 0";
                     score+=roll.GetAddedScore()=0;
                     break;
-                    case8: scoreString = "Black cave -70";
+                    case 8: scoreString = "Black cave -70";
                     score+=roll.GetAddedScore()=-70
                     break;
-                    case9: scoreString = "Huts in the mountain +60";
+                    case 9: scoreString = "Huts in the mountain +60";
                     score+=roll.GetAddedScore()=+60;
                     break;
-                    case10: scoreString = "The Werewall (werewolf-wall -80) bonustur";
+                    case 10: scoreString = "The Werewall (werewolf-wall -80) bonustur";
                     score+=roll.GetAddedScore()=-70;
                     break;
-                    case11: scoreString = "The pit -50";
+                    case 11: scoreString = "The pit -50";
                     score+=roll.GetAddedScore()=-50;
                     break;
-                    case12: scoreString = "Goldmine +650";
+                    case 12: scoreString = "Goldmine +650";
                     score+=roll.GetAddedScore("");
                     break;
                 }
