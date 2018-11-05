a29f2b1083c2e0ad78d6e3e989ca038f6ca22ca9
Fredrik <polostyre@hotmail.com>
2018-11-05 13:32:58 +0100

    Made a roll class and made some fixes to the player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,96 +1,96 @@
-package java;
 public class Player{
 
-        private static Roll roll = new Roll();
-        private static int numberOfPlayers = 0;
-        final String NAME;
+    private Roll roll = new Roll();
+    //It's the same for all the players, that's why we use static.
+    private static int numberOfPlayers = 0;
+    final String NAME;
 
-        private int score;
-        private boolean won;
-        private Roll lastRoll;
+    private int score;
+    private boolean won;
+    private Roll lastRoll;
 
-        private String rollLog;
+    private String rollLog;
 
 // using switch statement to AddScore
-    // switch engelsk
-        public void main() {
-            int roll = faceValue;
-                String scoreString;
-                switch (score) {
-                    case 1: scoreString = "invalid number";
-                    break;
-                    case 2: scoreString = "Tower + 250";
-                    score+=roll.GetAddedScore()=+250;
-                    break;
-                    case 3: scoreString = "Crater -100";
-                    score+=roll.GetAddedScore()=-100;
-                    break;
-                    case 4: scoreString = "Palace gates + 100";
-                    score+=roll.GetAddedScore()= +100;
-                    break;
-                    case 5: scoreString = "Cold Desert -20";
-                    score+=roll.GetAddedScore()=-20;
-                    break;
-                    case 6: scoreString = "Walled city +180";
-                    score+=roll.GetAddedScore()+180;
-                    break;
-                    case 7: scoreString = "Monastry 0";
-                    score+=roll.GetAddedScore()=0;
-                    break;
-                    case 8: scoreString = "Black cave -70";
-                    score+=roll.GetAddedScore()=-70
-                    break;
-                    case 9: scoreString = "Huts in the mountain +60";
-                    score+=roll.GetAddedScore()=+60;
-                    break;
-                    case 10: scoreString = "The Werewall (werewolf-wall -80) bonustur";
-                    score+=roll.GetAddedScore()=-70;
-                    break;
-                    case 11: scoreString = "The pit -50";
-                    score+=roll.GetAddedScore()=-50;
-                    break;
-                    case 12: scoreString = "Goldmine +650";
-                    score+=roll.GetAddedScore("");
-                    break;
-                }
-            }
 
-
-            lastRoll = roll;
-
-
-        public Player(){
-            won = false;
-            score = 0;
-            NAME = "Player " + ++numberOfPlayers;
-            rollLog = "";
-            lastRoll = roll;
-        }
-
-
-        public boolean GetWon(){
-            return won;
-        }
-
-        public boolean CheckWin(){
-            if (score >= 3000)
-                return true;
-           else
-                return false;
-        }
-
-        public String getRollLog() {
-            return rollLog;
-        }
-
-        public void setRollLog(String rollLog) {
-            this.rollLog = rollLog;
-        }
-
-        public int getScore() {
-            return score;
+    public void main() {
+        int roll = lastRoll.GetAddedScore();
+        String scoreString;
+        switch (score) {
+            case 1: scoreString = "invalid number";
+                break;
+            case 2: scoreString = "Tower + 250";
+                 score += 250;
+                break;
+            case 3: scoreString = "Crater -100";
+                score -= 100;
+                break;
+            case 4: scoreString = "Palace gates + 100";
+                score += 100;
+                break;
+            case 5: scoreString = "Cold Desert -20";
+                score -= 20;
+                break;
+            case 6: scoreString = "Walled city +180";
+                score += 180;
+                break;
+            case 7: scoreString = "Monastry 0";
+                score += 0;
+                break;
+            case 8: scoreString = "Black cave -70";
+                score -= 70;
+                break;
+            case 9: scoreString = "Huts in the mountain +60";
+                score += 60;
+                break;
+            case 10: scoreString = "The Werewall (werewolf-wall -80) bonustur";
+                score -= 70;
+                break;
+            case 11: scoreString = "The pit -50";
+                score -= 50;
+                break;
+            case 12: scoreString = "Goldmine +650";
+                score += 650;
+                break;
         }
     }
+
+
+
+    public Player(){
+        won = false;
+        score = 1000;
+        NAME = "Player " + ++numberOfPlayers;
+        rollLog = "";
+        lastRoll = roll;
+    }
+
+
+    public boolean GetWon(){
+        return won;
+    }
+
+    public boolean CheckWin(){
+        if (score >= 3000)
+            return true;
+        else
+            return false;
+    }
+
+    public String getRollLog() {
+        return rollLog;
+    }
+
+    public void setRollLog(String rollLog) {
+        this.rollLog = rollLog;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
+
+
 
 
 
---
+++ src/main/java/Roll.java
@@ -0,0 +1,27 @@
+import java.Die;
+
+public class Roll {
+    private int value1;
+    public int value2;
+    Die die = new Die();
+
+    public Roll(){
+        RollDice();
+    }
+
+    public void  RollDice(){
+        value1 = die.RollDie();
+        value2 = die.RollDie();
+    }
+
+    public int GetAddedScore(){
+        return value1 + value2;
+    }
+
+
+    public String toString() {
+        return "(" + value1 + ", " + value2 + ")";
+
+
+
+    }}
