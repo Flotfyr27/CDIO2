4a64e436ce4101e82a7700cab45771408ea6c7ea
Fredrik <polostyre@hotmail.com>
2018-11-06 11:14:59 +0100

    updating the switch and making some comments.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -11,52 +11,52 @@ public class Player{
 
     private String rollLog;
 
-// using switch statement to AddScore
+// using switch statement to add the score to player
 
     public void main() {
         int roll = lastRoll.GetAddedScore();
-        String scoreString;
-        switch (score) {
-            case 1: scoreString = "invalid number";
+        String besked;
+        switch (roll) {
+            case 1: String besked1= "invalid number";
                 break;
-            case 2: scoreString = "Tower + 250";
+            case 2: String besked2= "Tower + 250";
                  score += 250;
                 break;
-            case 3: scoreString = "Crater -100";
+            case 3: String besked3 = "Crater -100";
                 score -= 100;
                 break;
-            case 4: scoreString = "Palace gates + 100";
+            case 4: String besked4 = "Palace gates + 100";
                 score += 100;
                 break;
-            case 5: scoreString = "Cold Desert -20";
+            case 5: String besked5= "Cold Desert -20";
                 score -= 20;
                 break;
-            case 6: scoreString = "Walled city +180";
+            case 6: String besked6= "Walled city +180";
                 score += 180;
                 break;
-            case 7: scoreString = "Monastry 0";
+            case 7: String besked7= "Monastry 0";
                 score += 0;
                 break;
-            case 8: scoreString = "Black cave -70";
+            case 8: String besked8= "Black cave -70";
                 score -= 70;
                 break;
-            case 9: scoreString = "Huts in the mountain +60";
+            case 9: String besked9= "Huts in the mountain +60";
                 score += 60;
                 break;
-            case 10: scoreString = "The Werewall (werewolf-wall -80) bonustur";
+            case 10: String besked10= "The Werewall (werewolf-wall -80) bonustur";
                 score -= 70;
                 break;
-            case 11: scoreString = "The pit -50";
+            case 11: String besked11= "The pit -50";
                 score -= 50;
                 break;
-            case 12: scoreString = "Goldmine +650";
+            case 12: String besked12= "Goldmine +650";
                 score += 650;
                 break;
         }
     }
 
 
-
+//player start score set to 1000
     public Player(){
         won = false;
         score = 1000;
@@ -69,7 +69,7 @@ public class Player{
     public boolean GetWon(){
         return won;
     }
-
+//Checking if a player was won or not.
     public boolean CheckWin(){
         if (score >= 3000)
             return true;
