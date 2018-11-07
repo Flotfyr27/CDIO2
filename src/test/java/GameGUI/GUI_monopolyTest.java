c738775406a02c7e756461b2471f6f4ed16bc9ac
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2018-11-07 15:48:15 +0100

    Added test for movement of players

--- src/test/java/GameGUI/GUI_monopolyTest.java
+++ src/test/java/GameGUI/GUI_monopolyTest.java
@@ -3,5 +3,6 @@ package GameGUI;
 public class GUI_monopolyTest {
     public static void main(String[] args) {
         GUI_monopoly guiTest = new GUI_monopoly();
+        guiTest.movePlayer(5, guiTest.getPlayer(1));
     }
 }
